7e88ad9ff0af0c3b1d65bee1fc9a1c4e9b77560a
Keegan Smith <keeganasmith2003@gmail.com>
2025-06-20 11:45:46 -0500

    rid of getModels entirely

--- src/service/modelHandler.ts
+++ src/service/modelHandler.ts
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-import { getModels } from './api';
 import { StatusBarWidget } from '../StatusBarWidget';
 import { IModelInfo } from '../utils/schema';
 const myModelInfo: IModelInfo = {
