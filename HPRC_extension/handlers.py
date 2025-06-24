42f5b478a9fd6784c95b9a9c1cd5fa6a0c120dcb
Keegan Smith <keeganasmith2003@gmail.com>
2025-06-24 15:38:28 -0500

    remove web authenticated from endpoints

--- HPRC_extension/handlers.py
+++ HPRC_extension/handlers.py
@@ -4,14 +4,12 @@ from tornado import web
 import json
 
 class HelloHandler(APIHandler):
-    @web.authenticated
     def get(self):
         # You can access query args via self.get_argument(...)
         name = self.get_argument("name", default="world")
         # And respond with JSON automatically:
         self.finish({"message": f"Hello, {name}!"})
 
-    @web.authenticated
     def post(self):
         # Read JSON body
         body = self.get_json_body()
